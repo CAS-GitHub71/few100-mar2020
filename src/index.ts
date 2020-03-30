57ba5afe50e94e4139e56c98232564d04db7ef97
Christine Seavey <christineaseavey@yahoo.com>
2020-03-30 17:49:32 +0000

    html

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
 
+const items: ShoppingItem[] = [
+    { description: 'Tolilet Paper' },
+    { description: 'Beer' },
+    { description: 'Lysol' },
+    { description: 'Shampoo' }
+];
+
+const theList = document.getElementById('the-list') as HTMLUListElement;
+// <li class="alert alert-info"><span>Shampoo</span></li>
+drawList();
+function drawList() {
+    items.forEach(item => {
+        const newElement = document.createElement('li');
+        newElement.classList.add('alert');
+        newElement.classList.add('alert-info');
+        const newSpan = document.createElement('span');
+        newSpan.innerText = item.description;
+        newElement.appendChild(newSpan);
+        theList.appendChild(newElement);
+    });
+}
+
+interface ShoppingItem { description: string; }
